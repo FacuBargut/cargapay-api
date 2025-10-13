3d7614dfe58f9a0c9ba2de7fc72820e6e7575b83
Facu <facubargut@gmail.com>
2025-10-12 22:14:42 -0300

    vercel.json

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ dns.setDefaultResultOrder('ipv4first');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:3001',
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
